5165b13ed55bd94749bdf6ed34e8f6a508bbf8f2
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-03-17 09:28:01 +0100

    working UDP server

--- src/graphie/receiver.clj
+++ src/graphie/receiver.clj
@@ -3,14 +3,17 @@
   (:require [graphie.sockets :as sockets]))
 
 
-(defn- receive-data [in out]
+(defn- receive-stream-data [in out]
   (let [reader (java.io.PushbackReader. (java.io.InputStreamReader. in "utf8"))]
     (let [name (read reader)
           value (read reader)
           time (read reader)]
       (println (str "Got sample for " name ", value: " value ", time: " time)))))
 
+(defn- receive-udp [packet]
+  (println (str "Got packet '" (String. (.getData packet) (.getOffset packet) (.getLength packet) "utf-8") "'")))
+
 (defn start [port]
-  (sockets/start-server port receive-data))
+  (sockets/start-udp-server port receive-udp))
 
 
--- src/graphie/sockets.clj
+++ src/graphie/sockets.clj
@@ -1,14 +1,18 @@
 (ns
   graphie.sockets
-  (:import (java.net InetAddress ServerSocket Socket SocketException)
+  (:import (java.net InetAddress ServerSocket Socket SocketException DatagramSocket DatagramPacket)
+           (java.io IOException)
            (java.util.concurrent Executors)))
 
 (defonce executor (Executors/newCachedThreadPool))
 
-(defrecord server [socket clients])
-
 (defn- execute [^Runnable f]
   (.execute executor f))
+
+;
+; TCP server parts
+;
+(defrecord tcp-server [socket clients])
 
 (defn- close [^Socket s]
   (when-not (.isClosed s)
@@ -28,20 +32,35 @@
                   (close s)
                   (dosync (commute clients disj s))))))
 
-(defn- create-server [f ^ServerSocket ss]
+(defn- create-tcp-server [f ^ServerSocket ss]
   (let [clients (ref #{})]
     (execute #(when-not (.isClosed ss)
                   (try
                     (accept (.accept ss) clients f)
                     (catch SocketException e))
                   (recur)))
-    (server. ss clients)))
+    (tcp-server. ss clients)))
 
-(defn start-server [port f]
-    (create-server f (ServerSocket. port)))
+(defn start-tcp-server [port f]
+    (create-tcp-server f (ServerSocket. port)))
 
-(defn stop-server [server]
+(defn stop-tcp-server [server]
   (doseq [s @(:clients server)]
     (close s))
   (dosync (ref-set (:clients server) #{}))
   (.close ^ServerSocket (:socket server)))
+
+;
+; UDP parts
+;
+
+(defn start-udp-server [port f]
+  (let [ds (DatagramSocket. port)]
+    (execute #(when-not (.isClosed ds)
+                (try
+                  (let [dp (DatagramPacket. (byte-array 1024) 1024)]
+                    (.receive ds dp)
+                    (f dp))
+                    (catch IOException e))
+                (recur)))
+    (tcp-server. ds [])))
