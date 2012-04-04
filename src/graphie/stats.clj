a527325a9f0e21158ace1f4f5637e1b29525b901
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-04-04 16:16:52 +0200

    stable state, need to aggregate the times into sec-buckets now

--- src/graphie/stats.clj
+++ src/graphie/stats.clj
@@ -3,10 +3,25 @@
 
 (defonce stats-agent (agent {}))
 
+(defn seconds-from-nanos [nanos]
+  (long (Math/floor (/ nanos 1000000000))))
+
+(defn- create-key-stats [message]
+  {:in [(:value message)], :s (seconds-from-nanos (:time message))})
+
+(defn- add-to-in [message key-stats]
+  (if (= (:s key-stats) (seconds-from-nanos (:time message)))
+    (assoc key-stats :in (conj (get key-stats :in []) (:value message)))
+    (merge
+      key-stats
+      (create-key-stats message))))
+
 (defn process-timing-message [data message]
   (let [key (keyword (:name message))
-        values (get data key [])]
-    (assoc data (keyword (:name message)) (conj values (:value message)))))
+        key-stats (key data)]
+    (if (nil? key-stats)
+      (assoc data key (create-key-stats message))
+      (assoc data key (add-to-in message key-stats)))))
 
 (defn record-stats [message]
   (if (= "ms" (:type message))
--- test/graphie/statstest.clj
+++ test/graphie/statstest.clj
@@ -2,15 +2,34 @@
   (:use midje.sweet)
   (:use graphie.stats))
 
+(fact "123456 is second 0"
+  (seconds-from-nanos 123456)
+  => 0)
+
+(fact "2000000000 is second 2"
+  (seconds-from-nanos 2000000000)
+  => 2)
+
 (fact "First timing message gets inserted"
-  (process-timing-message {} {:name "request_time" :value 320 :type "ms"})
-  => {:request_time [320]})
+  (process-timing-message
+    {}
+    {:name "request_time" :value 320 :type "ms", :time 123456})
+  => {:request_time {:in [320], :s 0}})
 
 (fact "A second timing message gets added"
-  (process-timing-message {:request_time [320]} {:name "request_time" :value 640 :type "ms"})
-  => {:request_time [320 640]})
+  (process-timing-message
+    {:request_time {:in [320], :s 0}}
+    {:name "request_time" :value 640 :type "ms", :time 123457})
+  => {:request_time {:in [320 640], :s 0}})
 
 (fact "A different metric gets a new key"
-  (process-timing-message {:request_time [320]} {:name "response_time" :value 640 :type "ms"})
-  => {:request_time [320] :response_time [640]})
+  (process-timing-message
+    {:request_time {:in [320], :s 0}}
+    {:name "response_time" :value 640 :type "ms", :time 123458})
+  => {:request_time {:in [320], :s 0} :response_time {:in [640], :s 0}})
 
+(fact "Sending a new value for another second starts new value list"
+  (process-timing-message
+    {:request_time {:in [320], :s 0}}
+    {:name "request_time" :value 640 :type "ms", :time 2000000000})
+  => {:request_time {:in [640] :s 2}})
