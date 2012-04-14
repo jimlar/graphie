c46624700a12af25f901822ac813030a393d613f
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2012-04-14 20:39:25 +0200

    now calculating max, min, sum, avg and #samples for each metric/second

--- src/graphie/stats.clj
+++ src/graphie/stats.clj
@@ -14,10 +14,19 @@
 (defn values [name]
   (map second-to-point (:secs (name @stats-agent))))
 
-(defn- finish-second [stats value time]
+(defn- second-summary [values second]
+  (let [sum (reduce + values)]
+    {:second second
+     :avg (long (math/round (/ sum (count values))))
+     :sum sum
+     :max (reduce max values)
+     :min (reduce min values)
+     :samples (count values)}))
+
+(defn- start-second [stats value time]
   (merge
     (if (contains? stats :in)
-      (assoc stats :secs (conj (get stats :secs []) {:v (:in stats) :second (:second stats)}))
+      (assoc stats :secs (conj (get stats :secs []) (second-summary (:in stats) (:second stats))))
       {})
     {:in [value], :second (time/as-seconds time)}))
 
@@ -27,16 +36,16 @@
 (defn- same-second? [message stats]
   (= (:second stats) (time/as-seconds (:time message))))
 
-(defn add-value-message [data message]
+(defn merge-message [data message]
   (let [key (keyword (:name message))
         stats (key data {})]
     (assoc data key
       (if (same-second? message stats)
         (add-value stats (:value message))
-        (finish-second stats (:value message) (:time message))))))
+        (start-second stats (:value message) (:time message))))))
 
 (defn record-stats [message]
   (if (= "v" (:type message))
-    (send stats-agent add-value-message message)
+    (send stats-agent merge-message message)
     (throw (IllegalArgumentException. (str "Unsupported message type: " (:type message))))))
 
--- test/graphie/statstest.clj
+++ test/graphie/statstest.clj
@@ -3,31 +3,32 @@
   (:use graphie.stats))
 
 (fact "First timing message gets inserted"
-  (add-value-message
+  (merge-message
     {}
     {:name "request_time" :value 320 :type "v", :time 456})
   => {:request_time {:in [320], :second 0}})
 
 (fact "A second timing message gets added"
-  (add-value-message
+  (merge-message
     {:request_time {:in [320], :second 0}}
     {:name "request_time" :value 640 :type "v", :time 457})
   => {:request_time {:in [320 640], :second 0}})
 
 (fact "A different metric gets a new key"
-  (add-value-message
+  (merge-message
     {:request_time {:in [320], :second 0}}
     {:name "response_time" :value 640 :type "v", :time 458})
   => {:request_time {:in [320], :second 0} :response_time {:in [640], :second 0}})
 
 (fact "A new value for another second starts new value list and summarizes the previous second"
-  (add-value-message
+  (merge-message
     {:request_time {:in [320 456], :second 0}}
     {:name "request_time" :value 640 :type "v", :time 2000})
-  => {:request_time {:in [640] :second 2 :secs [{:v [320 456], :second 0}]}})
+  => {:request_time {:in [640] :second 2 :secs [{:avg 388, :sum 776, :max 456, :min 320, :samples 2, :second 0}]}})
 
 (fact "A new value for a third  second starts adds to the seconds list"
-  (add-value-message
-    {:request_time {:in [640] :second 2 :secs [{:v [320 456], :second 0}]}}
+  (merge-message
+    {:request_time {:in [640] :second 2 :secs [{:avg 388, :sum 776, :max 456, :min 320, :samples 2, :second 0}]}}
     {:name "request_time" :value 720 :type "v", :time 3000})
-  => {:request_time {:in [720] :second 3 :secs [{:v [320 456], :second 0} {:v [640], :second 2}]}})
+  => {:request_time {:in [720] :second 3 :secs [{:avg 388, :sum 776, :max 456, :min 320, :samples 2, :second 0}
+                                                {:avg 640, :sum 640, :max 640, :min 640, :samples 1, :second 2}]}})
