a34af296c99746195edfa0eac4da1c74d6f5d345
Jimmy Larsson <jimmy.k.larsson@gmail.com>
2013-03-10 14:01:52 +0100

    replaced noir with compojure and bumped clojure to 1.5

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
 (defproject graphie "0.1.0-SNAPSHOT"
             :description "Small statistics server"
-            :dependencies [[org.clojure/clojure "1.3.0"]
+            :dependencies [[org.clojure/clojure "1.5.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
-                           [noir "1.2.2"]
+                           [compojure "1.1.5"]
+                           [hiccup "1.0.2"]
+                           [clj-json "0.4.3"]
                            [com.novemberain/monger "1.0.0-beta5"]]
-            :repositories {"stuart" "http://stuartsierra.com/maven2"}
-            :main graphie.main
+            :plugins [[lein-ring "0.8.2"]]
+            :ring {
+              :init graphie.main/init
+              :handler graphie.main/app
+            }
             :profiles {
               :dev {
-                :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
-              }
-              :test {
-                :dependencies [
-                  [midje "1.4.0"]
-                  [com.stuartsierra/lazytest "1.2.3"]
-                ]
+                :dependencies [[midje "1.5.0"]]
+                :plugins [[lein-midje "3.0.0"]]
               }
             })
--- src/graphie/main.clj
+++ src/graphie/main.clj
@@ -1,14 +1,67 @@
 (ns graphie.main
-  (:require [noir.server :as noir]
+  (:use compojure.core
+        [hiccup.def :only [defhtml]]
+        [hiccup.page :only [include-css include-js html5]])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [clj-json.core :as json]
+            [clojure.string :as string]
             [graphie.receiver :as receiver]
-            [graphie.stats :as stats]))
+            [graphie.stats :as stats]
+            [graphie.storage :as storage]))
 
-(noir/load-views "src/graphie/views/")
+(defhtml layout [theme & content]
+  (let [theme (if (empty? theme) "spacelab" theme)]
+    (html5
+      [:head
+       [:title "graphie"]
+       (include-css "/css/reset.css")
+       (include-css (str "/bootstrap/themes/" theme "/bootstrap.min.css"))]
+      [:body
+       [:div#wrapper content]
+       (include-js "/js/jquery.min.js")
+       (include-js "/js/jquery.flot.min.js")
+       (include-js "/bootstrap/js/bootstrap.js")
+       (include-js "/js/graphie.js")
+       ])))
 
-(defn -main [& m]
-  (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))
-        receiver (receiver/start 7890 stats/record-stats)]
-    (noir/start port {:mode mode :ns 'graphie})
-    (println "Started")
-    receiver))
+(defn index [theme]
+  (layout
+    theme
+    [:h1 "Welcome to graphie"]
+    [:h2 "Current data"]
+    [:div#placeholder {:style "width:1024px;height:600px;"}]))
+
+(defn- point-for-key [key second]
+  [(* (:second second) 1000) (key second)])
+
+(defn- pretty [name]
+  (string/capitalize (string/replace (string/replace (str name) ":" "" ) "_" " ")))
+
+(defn- data-set [name]
+  (let [values (storage/seconds name)
+        pretty-name (pretty name)]
+    [
+      {:label (str pretty-name " average") :data (map (partial point-for-key :average) values)}
+      {:label (str pretty-name " sum") :data (map (partial point-for-key :sum) values)}
+      {:label (str pretty-name " max") :data (map (partial point-for-key :max) values)}
+      {:label (str pretty-name " min") :data (map (partial point-for-key :min) values)}
+      {:label (str pretty-name " #samples") :data (map (partial point-for-key :samples) values)}
+      ]))
+
+(defn graph-data []
+  {:headers {"Content-Type" "application/json"}
+    :body (json/generate-string (reduce concat (map data-set (storage/names))))})
+
+(defroutes app-routes
+  (GET "/" [theme] (index theme))
+  (GET "/data.json" [] (graph-data))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
+
+(defn init []
+  (receiver/start 7890 stats/record-stats)
+  (println "Receiver started"))
